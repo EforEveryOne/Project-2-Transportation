a768630988372f0199d0a836615e3388dac01050
EforEveryOne <31865908+EforEveryOne@users.noreply.github.com>
2017-10-23 01:55:11 -0400

    Add files via upload

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    public class CubeBehavior : MonoBehaviour {
+	public GameObject cubePrefab;
+// When the mouse clickes down on the object, the things below will happen.
+	void OnMouseDown () {
+// if the red cube is false it'll turn white		
+		if (MainScript.redCube != null) {
+			MainScript.redCube.GetComponent<Renderer> ().material.color = Color.white;
+		}
+// Turns the clicked cube red and assigns it the redcube
+		gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		MainScript.redCube = gameObject;
+	}
+}
---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MainScript : MonoBehaviour {
+// Varriables are defined here.
+    public static GameObject currentCube;
+	public static GameObject redCube;
+	int xPosition;
+	Vector3 cubePosition;
+	GameObject[]cubeRow = new GameObject [16];
+	public GameObject cubePrefab;
+// This is where things start to happen.
+	void Start () {
+// Spacing between the cubes.		
+		xPosition = 2;
+		// Tells the cubes where to be and when to be (right away).
+		cubePosition = new Vector3 (-15, 0, 0);
+		for (int i = 0; i < 16; i++) {
+			cubeRow[i] = (GameObject)Instantiate (cubePrefab, cubePosition, Quaternion.identity);
+			cubePosition += new Vector3 (xPosition, 0, 2);
+// Makes the cubes white.
+			cubeRow[i].GetComponent<Renderer> ().material.color = Color.white;
+		}
+	}
+// Where things may happen in the future of this project.
+	void Update () {
+	}
+}
